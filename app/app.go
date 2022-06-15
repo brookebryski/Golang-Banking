962a2f9c03540fd46632ebe744bee61fe871b0a3
BrookeBryski <brookeabryski@gmail.com>
2022-06-15 15:18:40 -0700

    started find by id logic

--- app/app.go
+++ app/app.go
@@ -16,6 +16,7 @@ func Start() {
 	ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryDb())}
 	// define routes
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 	// starting server
 	log.Fatalln(http.ListenAndServe("localhost:8000", router))
 }
