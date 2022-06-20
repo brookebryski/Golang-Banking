559d0b058bd51c9e110b6634da5f66382e9de3a6
BrookeBryski <brookeabryski@gmail.com>
2022-06-20 14:35:58 -0700

    started new account logic and db configuration

--- app/app.go
+++ app/app.go
@@ -4,8 +4,10 @@ import (
 	"Golang-Banking/domain"
 	"Golang-Banking/service"
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 	"log"
 	"net/http"
+	"time"
 )
 
 //func sanityCheck() {
@@ -23,7 +25,10 @@ func Start() {
 
 	// wiring
 	//ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
-	ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryDb())}
+	dbClient := getDbClient()
+	customerRepositoryDb := domain.NewCustomerRepositoryDb(dbClient)
+	//accountRepositoryDb := domain.NewAccountRepositoryDb(dbClient)
+	ch := CustomerHandlers{service.NewCustomerService(customerRepositoryDb)}
 	// define routes
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
@@ -32,4 +37,24 @@ func Start() {
 	//port := os.Getenv("SERVER_PORT")
 	//log.Fatalln(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 	log.Fatalln(http.ListenAndServe("localhost:8000", router))
+}
+
+func getDbClient() *sqlx.DB {
+	//dbUser := os.Getenv("DB_USER")
+	//dbPasswd := os.Getenv("DB_PASSWD")
+	//dbAddr := os.Getenv("DB_ADDR")
+	//dbPort := os.Getenv("DB_PORT")
+	//dbName := os.Getenv("DB_NAME")
+	//
+	//dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/$s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
+	//client, err := sqlx.Open("mysql", dataSource)
+	client, err := sqlx.Open("mysql", "root:Bb080695@tcp(localhost:3306)/banking")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+	return client
 }
--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -6,7 +6,6 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-	"time"
 )
 
 type CustomerRepositoryDb struct {
@@ -46,22 +45,6 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 	return &c, nil
 }
 
-func NewCustomerRepositoryDb() CustomerRepositoryDb {
-	//dbUser := os.Getenv("DB_USER")
-	//dbPasswd := os.Getenv("DB_PASSWD")
-	//dbAddr := os.Getenv("DB_ADDR")
-	//dbPort := os.Getenv("DB_PORT")
-	//dbName := os.Getenv("DB_NAME")
-
-	//dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/$s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
-	//client, err := sqlx.Open("mysql", dataSource)
-	client, err := sqlx.Open("mysql", "root:Bb080695@tcp(localhost:3306)/banking")
-	if err != nil {
-		panic(err)
-	}
-	// See "Important settings" section.
-	client.SetConnMaxLifetime(time.Minute * 3)
-	client.SetMaxOpenConns(10)
-	client.SetMaxIdleConns(10)
-	return CustomerRepositoryDb{client}
+func NewCustomerRepositoryDb(dbClient *sqlx.DB) CustomerRepositoryDb {
+	return CustomerRepositoryDb{dbClient}
 }
